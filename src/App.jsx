e05f4f11a8c1cf6331e179f1b90a9704d084267a
ilyasworks <ilyasghavis@gmail.com>
2024-02-06 14:59:25 +0500

    set router dom nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Services from './Pages/Services/Services';
+import ContactUs from './Pages/Contact Us/ContactUs';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Router>
+        <nav className='naveContinar' >
+          <div>
+           <h1> INTOUCH SOLUTION </h1>
+          </div>
+          <div className='pages'>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">Services</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
-export default App
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<Services />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
